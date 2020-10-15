884d91e21cead72a628b9ceba5d4a5164705a7da
Darshak Parikh <darshak@protonmail.com>
2020-10-15 18:41:07 +0530

    Run Prettier

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,28 @@
-import pkg from './load-package.cjs';
+import pkg from "./load-package.cjs";
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/main.mjs',
-		output: {
-			name: 'cyclist',
-			file: pkg.browser,
-			format: 'umd'
-		}
-	},
+  // browser-friendly UMD build
+  {
+    input: "src/main.mjs",
+    output: {
+      name: "cyclist",
+      file: pkg.browser,
+      format: "umd",
+    },
+  },
 
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'src/main.mjs',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: "src/main.mjs",
+    external: ["ms"],
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+    ],
+  },
 ];
