c9d54ef86936d6b62298c3abe155c13176ec33b4
Darshak Parikh <darshak@protonmail.com>
2020-10-16 11:54:27 +0530

    Implement `add()` and `toArray()`

--- src/main.mjs
+++ src/main.mjs
@@ -1,3 +1,13 @@
-class Cyclist {}
+class Cyclist {
+  elements = [];
+
+  add(element) {
+    this.elements.push(element);
+  }
+
+  toArray() {
+    return this.elements;
+  }
+}
 
 export default Cyclist;
--- test/test.js
+++ test/test.js
@@ -4,3 +4,15 @@ import Cyclist from "..";
 test("Cyclist is exported correctly", (t) => {
   t.is(typeof Cyclist, "function");
 });
+
+test("returns an array on toArray()", (t) => {
+  const list = new Cyclist();
+  t.true(Array.isArray(list.toArray()));
+});
+
+test("adds an element on add()", (t) => {
+  const list = new Cyclist();
+  list.add(42);
+  list.add(16);
+  t.deepEqual(list.toArray(), [42, 16]);
+});
