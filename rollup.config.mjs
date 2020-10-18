d1aad9aae2013ce01199494d37793d81194e9288
Darshak Parikh <darshak@protonmail.com>
2020-10-18 22:46:18 +0530

    Update main.js to support XO

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import pkg from "./load-package.cjs";
 export default [
   // browser-friendly UMD build
   {
-    input: "src/main.mjs",
+    input: "src/main.js",
     output: {
       name: "cyclist",
       file: pkg.browser,
@@ -18,7 +18,7 @@ export default [
   // an array for the `output` option, where we can specify
   // `file` and `format` for each target)
   {
-    input: "src/main.mjs",
+    input: "src/main.js",
     external: ["ms"],
     output: [
       { file: pkg.main, format: "cjs", exports: "default" },
---
+++ src/main.js
@@ -0,0 +1,39 @@
+class CyclistIterator {
+	constructor(elements) {
+		this.elements = elements;
+		this.current = -1;
+	}
+
+	cycleBack() {
+		this.current = this.current === this.elements.length ? 0 : this.current;
+		this.current =
+			this.current < 0 ? this.elements.length - 1 : this.current;
+	}
+
+	next() {
+		this.current++;
+		this.cycleBack();
+		return {value: this.elements[this.current], done: false};
+	}
+
+	previous() {
+		this.current--;
+		this.cycleBack();
+		return {value: this.elements[this.current], done: false};
+	}
+
+	nextValue() {
+		return this.next().value;
+	}
+
+	previousValue() {
+		return this.previous().value;
+	}
+}
+
+function cyclist(iterable) {
+	const elements = Array.from(iterable);
+	return new CyclistIterator(elements);
+}
+
+export default cyclist;
